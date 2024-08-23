c150263e484adbdae68ac6cd17b48ab2dada875c
Nitin Kumar <nitin84498@gmail.com>
2024-08-23 11:05:02 +0530

    origin url updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobhunt-backend-cvka.onrender.com',
+    origin:'https://jobhunt-frontend-seven.vercel.app',
     credentials:true
 }
 
